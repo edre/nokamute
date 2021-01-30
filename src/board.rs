dfcfbbc020b70a7fa8e084ffe4b8218c0e208fca
Eric Roshan-Eisner <eric.d.eisner@gmail.com>
2021-01-30 12:30:18 -0800

    Add most of the movement logic.

--- src/board.rs
+++ src/board.rs
@@ -5,6 +5,8 @@ use std::collections::{HashMap, HashSet};
 use std::default::Default;
 use std::fmt::{Display, Formatter, Result};
 
+// TODO benchmarks: placement heavy starting from empty board; movement-heavy starting from full board
+
 // Ideas for board representation:
 // 1) Grid based: Keep a mostly empty grid with entries for what's in each cell.
 //      The grid will need to expand and/or translate if the hive gets too long or moves.
@@ -13,6 +15,9 @@ use std::fmt::{Display, Formatter, Result};
 //      Even computing adjacent nodes may require walking all the way through the other pieces...
 // 3) Location based: Hashmap or other association from coordinates to piece.
 //      Don't need to resize the grid, doesn't take more space than necessary.
+// 4) Graph based with grid backup.
+//      Dynamically allocate used and empty adjacent hexes with indexes.
+//      Compact adjacency list for each node. Generate new nodes when expanding.
 
 // Location-based model.
 // Hex coordinates. Grid connections plus one of the diagonals. First bug is at (0,0).
@@ -20,7 +25,8 @@ pub type Loc = (i8, i8);
 
 fn adjacent(loc: Loc) -> [Loc; 6] {
     let (x, y) = loc;
-    [(x, y - 1), (x - 1, y), (x, y + 1), (x + 1, y), (x - 1, y - 1), (x + 1, y + 1)]
+    // In clockwise order
+    [(x - 1, y - 1), (x, y - 1), (x + 1, y), (x + 1, y + 1), (x, y + 1), (x - 1, y)]
 }
 
 #[derive(Copy, Clone, Debug, Eq, PartialEq)]
@@ -115,39 +121,6 @@ impl Board {
     fn queen_required(&self) -> bool {
         self.move_num > 5 && self.get_remaining()[0] > 0
     }
-
-    fn generate_placements(&self, board_moves: &mut [Option<Move>], n: &mut usize) {
-        // First find empty spaces next to the correct color bugs.
-        let mut available = HashSet::new();
-        for (&loc, tile) in self.grid.iter() {
-            if tile.color != self.to_move() {
-                continue;
-            }
-            for &pos in adjacent(loc).iter() {
-                if self.get(pos).is_none() {
-                    available.insert(pos);
-                }
-            }
-        }
-
-        // Use empty spaces that have no opposite colored tiles adjacent.
-        for &pos in available.iter() {
-            let placeable = adjacent(pos)
-                .iter()
-                .all(|adj| self.get(*adj).map(|tile| tile.color == self.to_move()).unwrap_or(true));
-            if placeable {
-                for (bug, num_left) in self.get_available_bugs().iter() {
-                    if self.queen_required() && *bug != Bug::Queen {
-                        continue;
-                    }
-                    if *num_left > 0 {
-                        board_moves[*n] = Some(Move::Place(pos, *bug));
-                        *n += 1;
-                    }
-                }
-            }
-        }
-    }
 }
 
 #[test]
@@ -197,7 +170,7 @@ impl Board {
             miny = std::cmp::min(miny, y);
             maxy = std::cmp::max(maxy, y);
         }
-        (minx, maxx - minx + 1, miny, maxy - minx + 1)
+        (minx, maxx - minx + 1, miny, maxy - miny + 1)
     }
 
     fn fancy_fmt(&self) -> String {
@@ -243,7 +216,7 @@ impl Board {
     }
 }
 
-#[derive(Copy, Clone, Eq, PartialEq)]
+#[derive(Copy, Clone, Debug, Eq, PartialEq)]
 pub enum Move {
     Place(Loc, Bug),
     Movement(Loc, Loc),
@@ -318,6 +291,226 @@ impl minimax::Move for Move {
     }
 }
 
+impl Board {
+    fn generate_placements(&self, board_moves: &mut [Option<Move>], n: &mut usize) {
+        // First find empty spaces next to the correct color bugs.
+        let mut available = HashSet::new();
+        for (&loc, tile) in self.grid.iter() {
+            if tile.color != self.to_move() {
+                continue;
+            }
+            for &pos in adjacent(loc).iter() {
+                if self.get(pos).is_none() {
+                    available.insert(pos);
+                }
+            }
+        }
+
+        // Use empty spaces that have no opposite colored tiles adjacent.
+        for &pos in available.iter() {
+            let placeable = adjacent(pos)
+                .iter()
+                .all(|adj| self.get(*adj).map(|tile| tile.color == self.to_move()).unwrap_or(true));
+            if placeable {
+                for (bug, num_left) in self.get_available_bugs().iter() {
+                    if self.queen_required() && *bug != Bug::Queen {
+                        continue;
+                    }
+                    if *num_left > 0 {
+                        board_moves[*n] = Some(Move::Place(pos, *bug));
+                        *n += 1;
+                    }
+                }
+            }
+        }
+    }
+
+    // TODO: Linear algorithm to find all cut vertexes:
+    // Algorithm explanation: https://web.archive.org/web/20180830110222/https://www.eecs.wsu.edu/~holder/courses/CptS223/spr08/slides/graphapps.pdf
+    // Example code: https://cp-algorithms.com/graph/cutpoints.html
+    //
+    // TODO: cache movability for each tile, and somehow iteratively update it
+    // Need to persist the DFS tree from an arbitrary root.
+    // Adding a tile just adds a leaf to one of its neighbors
+    // Removing a tile means recomputing a path to the root for any children of the removed node.
+    // Hmm, maybe not. DFS iteration order is important.
+    fn is_cut_vertex(&self, loc: Loc) -> bool {
+        let mut visited = HashSet::new();
+        visited.insert(loc);
+        // Start searching from one arbitrary neighbor.
+        // This should never be called on a disconnected node.
+        let start: Loc =
+            *adjacent(loc).iter().filter(|adj| self.get(**adj).is_some()).next().unwrap();
+        let mut queue = vec![start];
+        while let Some(node) = queue.pop() {
+            if visited.contains(&node) {
+                continue;
+            }
+            visited.insert(node);
+            for &adj in adjacent(node).iter() {
+                if self.get(adj).is_some() {
+                    queue.push(adj);
+                }
+            }
+        }
+        visited.len() != self.grid.len()
+    }
+
+    // For a position on the outside (whether occupied or not), find all
+    // adjacent locations still connected to the hive that are slideable.
+    // A slideable position has 2 empty slots next to an occupied slot.
+    // For all 2^6 possibilities, there can be 0, 2, or 4 slideable neighbors.
+    fn slideable_adjacent(&self, loc: Loc) -> [Option<Loc>; 4] {
+        let mut out = [None; 4];
+        let mut n = 0;
+        let neighbors = adjacent(loc);
+        // Each bit is whether neighbor is occupied.
+        let mut occupied = 0;
+        for neighbor in neighbors.iter().rev() {
+            occupied <<= 1;
+            if self.get(*neighbor).is_some() {
+                occupied |= 1;
+            }
+        }
+        // Wrap around in each direction
+        occupied |= occupied << 6;
+        occupied = (occupied << 1) | (occupied >> 5) & 1;
+        let mut slideable = !occupied & (occupied << 1 ^ occupied >> 1);
+
+        for neighbor in &neighbors {
+            slideable >>= 1;
+            if slideable & 1 != 0 {
+                out[n] = Some(*neighbor);
+                n += 1;
+            }
+        }
+
+        out
+    }
+
+    // From any bug on top of a stack. Walk or jump down in any direction.
+    fn generate_stack_walking(&self, loc: Loc, board_moves: &mut [Option<Move>], n: &mut usize) {
+        for &adj in adjacent(loc).iter() {
+            if self.get(adj).is_none() {
+                board_moves[*n] = Some(Move::Movement(loc, adj));
+                *n += 1;
+            }
+        }
+    }
+
+    // Jumping over contiguous linear lines of tiles.
+    fn generate_jumps(&self, loc: Loc, board_moves: &mut [Option<Move>], n: &mut usize) {
+        for &dir in adjacent(loc).iter() {
+            if self.get(dir).is_some() {
+                let dx = dir.0 - loc.0;
+                let dy = dir.1 - loc.1;
+                let mut x = dir.0 + dx;
+                let mut y = dir.1 + dy;
+                while self.get((x, y)).is_some() {
+                    x += dx;
+                    y += dy;
+                }
+                board_moves[*n] = Some(Move::Movement(loc, (x, y)));
+                *n += 1;
+            }
+        }
+    }
+
+    fn generate_walk1(&self, loc: Loc, board_moves: &mut [Option<Move>], n: &mut usize) {
+        // TODO
+    }
+    fn generate_walk3(&self, loc: Loc, board_moves: &mut [Option<Move>], n: &mut usize) {
+        // TODO
+    }
+    fn generate_walk_all(&self, loc: Loc, board_moves: &mut [Option<Move>], n: &mut usize) {
+        // TODO
+    }
+
+    fn generate_movements(&self, board_moves: &mut [Option<Move>], n: &mut usize) {
+        for (&loc, tile) in self.grid.iter() {
+            if tile.underneath.is_some() {
+                self.generate_stack_walking(loc, board_moves, n);
+            } else if !self.is_cut_vertex(loc) {
+                match tile.bug {
+                    Bug::Queen => self.generate_walk1(loc, board_moves, n),
+                    Bug::Grasshopper => self.generate_jumps(loc, board_moves, n),
+                    Bug::Spider => self.generate_walk3(loc, board_moves, n),
+                    Bug::Ant => self.generate_walk_all(loc, board_moves, n),
+                    Bug::Beetle => self.generate_walk1(loc, board_moves, n),
+                }
+            }
+        }
+    }
+}
+
+#[test]
+fn test_cut_vertex() {
+    let mut board = Board::default();
+    //．．🐝🐝🐝🐝
+    // ．．．🐝．🐝🐝
+    //．．．．🐝🐝
+    for &node in &[(0, 0), (0, 1), (1, 0), (2, 1), (1, 2), (2, 2), (-1, 0), (-2, 0), (3, 1)] {
+        board.insert(node, Bug::Queen, Color::Black);
+    }
+    println!("{}", board);
+    // Line 1
+    assert!(board.is_cut_vertex((-1, 0)));
+    assert!(!board.is_cut_vertex((-2, 0)));
+    assert!(!board.is_cut_vertex((0, 0)));
+    assert!(!board.is_cut_vertex((1, 0)));
+    // Line 2
+    assert!(!board.is_cut_vertex((0, 1)));
+    assert!(board.is_cut_vertex((2, 1)));
+    assert!(!board.is_cut_vertex((3, 1)));
+    // Line 3
+    assert!(!board.is_cut_vertex((1, 2)));
+    assert!(!board.is_cut_vertex((2, 2)));
+}
+
+#[test]
+fn test_slideable() {
+    let mut board = Board::default();
+    // One neighbor.
+    board.insert((0, 0), Bug::Queen, Color::Black);
+    board.insert((1, 0), Bug::Queen, Color::Black);
+    assert_eq!([Some((0, -1)), Some((1, 1)), None, None], board.slideable_adjacent((0, 0)));
+    // Two adjacent neighbors.
+    board.insert((1, 1), Bug::Queen, Color::Black);
+    assert_eq!([Some((0, -1)), Some((0, 1)), None, None], board.slideable_adjacent((0, 0)));
+    // Four adjacent neighbors.
+    board.insert((0, 1), Bug::Queen, Color::Black);
+    board.insert((-1, 0), Bug::Queen, Color::Black);
+    assert_eq!([Some((-1, -1)), Some((0, -1)), None, None], board.slideable_adjacent((0, 0)));
+    // Five adjacent neighbors.
+    board.insert((-1, -1), Bug::Queen, Color::Black);
+    assert_eq!([None, None, None, None], board.slideable_adjacent((0, 0)));
+    // 2 separated groups of neighbors.
+    board.remove((0, 1));
+    assert_eq!([None, None, None, None], board.slideable_adjacent((0, 0)));
+    // 2 opposite single neighbors
+    board.remove((1, 1));
+    board.remove((-1, -1));
+    assert_eq!(
+        [Some((-1, -1)), Some((0, -1)), Some((1, 1)), Some((0, 1))],
+        board.slideable_adjacent((0, 0))
+    );
+}
+
+#[test]
+fn test_generate_jumps() {
+    let mut board = Board::default();
+    for &node in &[(0, 0), (0, 1), (0, 3), (1, 0), (2, 0)] {
+        board.insert(node, Bug::Grasshopper, Color::Black);
+    }
+    let mut moves = [None; 6];
+    let mut n = 0;
+    board.generate_jumps((0, 0), &mut moves, &mut n);
+    assert_eq!(n, 2);
+    moves[..2].sort();
+    assert_eq!(moves[0], Some(Move::Movement((0, 0), (0, 2))));
+    assert_eq!(moves[1], Some(Move::Movement((0, 0), (3, 0))));
+}
+
 pub struct Game;
 
 impl minimax::Game for Game {
@@ -348,10 +541,8 @@ impl minimax::Game for Game {
             return n;
         }
 
-        // TODO: Identify pieces that are free to move.
-        // TODO: cache movability for each tile, and somehow iteratively update it
-
-        // TODO: For moveable pieces, generate all legal moves.
+        // For moveable pieces, generate all legal moves.
+        board.generate_movements(board_moves, &mut n);
 
         board_moves[n] = None;
         n
