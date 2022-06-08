191919e6f704cef91f06aaf1722c15074b1d9955
Eric Roshan-Eisner <eric.d.eisner@gmail.com>
2022-06-07 22:27:06 -0700

    Update release script.

--- release.sh
+++ release.sh
@@ -1,16 +1,13 @@
 sudo apt install mingw-w64
 
-echo "[profile.release]" >> Cargo.toml
-echo "lto = true" >> Cargo.toml
-echo "codegen-units = 1" >> Cargo.toml
+export CARGO_PROFILE_RELEASE_LTO=true
+export CARGO_PROFILE_RELEASE_CODEGEN_UNITS=1
 
 cargo build --release
-mv target/release/nokamute nokamute_linux_amd64
-strip nokamute_linux_amd64
-gzip -f nokamute_linux_amd64
+mv target/release/nokamute .
+strip nokamute
+tar czf nokamute_linux_amd64.tar.gz --owner=0 --group=0 nokamute
 
 cargo build --release --target x86_64-pc-windows-gnu
 mv target/x86_64-pc-windows-gnu/release/nokamute.exe .
 zip nokamute_win64.zip nokamute.exe
-
-git checkout Cargo.toml
